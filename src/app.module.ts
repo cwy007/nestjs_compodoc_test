062f7b9446d626587174a621109eac76ea2215db
chanweiyan <chanweiyan007@gmail.com>
2026-05-11 17:16:51 +0800

    fix: create database email_login_test default character set utf8mb4;

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,34 @@ import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
 import { BbbModule } from './bbb/bbb.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { authPlugins } from 'mysql2';
 
 @Module({
-  imports: [AaaModule, BbbModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Cwy17824',
+      database: 'email_login_test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugins: {
+          sha256_password: authPlugins.sha256_password,
+        }
+      }
+    }),
+    AaaModule,
+    BbbModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
