86f63588eb43a031514d0760ef40391f2ac549aa
chanweiyan <chanweiyan007@gmail.com>
2026-05-11 17:08:34 +0800

    fix: nest g resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AaaModule } from './aaa/aaa.module';
 import { BbbModule } from './bbb/bbb.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AaaModule, BbbModule],
+  imports: [AaaModule, BbbModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
